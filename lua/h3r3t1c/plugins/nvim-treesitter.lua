a2630c9dae238d0d060646cd1a55dd4295bb7027
Luis Felipe Dominguez Vega <ldominguezvega@gmail.com>
2022-06-11 01:10:37 -0400

    Add nix to TreeSitter ensure syntax installed
    Signed-off-by: Luis Felipe Dominguez Vega <ldominguezvega@gmail.com>

--- lua/h3r3t1c/plugins/nvim-treesitter.lua
+++ lua/h3r3t1c/plugins/nvim-treesitter.lua
@@ -1,7 +1,7 @@
 local ts = require("nvim-treesitter.configs")
 
 ts.setup({
-    ensure_installed = {"rust", "java", "yaml", "toml", "json", "bash", "hcl", "javascript", "lua", "python"},
+    ensure_installed = {"nix", "rust", "java", "yaml", "toml", "json", "bash", "hcl", "javascript", "lua", "python"},
     indent = { enable = true },
     playground = { enable = true },
     context_commentstring = { enable = true },
